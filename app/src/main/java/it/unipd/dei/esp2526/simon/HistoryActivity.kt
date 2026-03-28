7e7f9997ce6f81e2b154e29dbb0bd24789a7e4ab
alessiomodonesi <alessiomodo2004@gmail.com>
2026-03-28 12:04:41 +0100

    aggiunta classe kotlin per la seconda activity

---
+++ app/src/main/java/it/unipd/dei/esp2526/simon/HistoryActivity.kt
@@ -0,0 +1,32 @@
+package it.unipd.dei.esp2526.simon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import it.unipd.dei.esp2526.simon.ui.theme.SimonTheme
+
+class HistoryActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SimonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SecondScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(modifier: Modifier = Modifier) {
+    Text(text = "Schermata 2", modifier = modifier)
+}
