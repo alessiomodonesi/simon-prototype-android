865f27fcc731b0b5f37bd8aa16cb4357c0958748
alessiomodonesi <alessiomodo2004@gmail.com>
2026-03-28 16:04:20 +0100

    aggiunto scheletro per la 2a schermata, mancano modifier e lista reale

--- app/src/main/java/it/unipd/dei/esp2526/simon/HistoryActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2526/simon/HistoryActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -18,13 +16,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import it.unipd.dei.esp2526.simon.ui.theme.SimonTheme
 
 class HistoryActivity : ComponentActivity() {
@@ -43,5 +43,73 @@ class HistoryActivity : ComponentActivity() {
 
 @Composable
 fun SecondScreen(modifier: Modifier = Modifier) {
-    Text(text = "Schermata 2", modifier = modifier)
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // creare le reference <=> creare gli ID nella classe View
+        val (titleText, historyList) = createRefs()
+
+        // titolo
+        Text(
+            text = stringResource(R.string.history_title),
+            modifier = Modifier.constrainAs(titleText) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+            }
+        )
+
+        // lista dinamica
+        LazyColumn(
+            modifier = Modifier.constrainAs(historyList) {
+                top.linkTo(titleText.bottom, margin = 16.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            },
+        ) {
+            items(1) { sequence ->
+                GameHistoryRow(sequence = "R, G, B")
+            }
+        }
+    }
+}
+
+@Composable
+fun GameHistoryRow(sequence: String) {
+    // riga che contiene le informazioni di una singola partita
+    ConstraintLayout(modifier = Modifier) {
+        // creare le reference <=> creare gli ID nella classe View
+        val (sizeText, sequenceText) = createRefs()
+
+        // numero di rettangoli premuti
+        Text(
+            text = "3",
+            modifier = Modifier.constrainAs(sizeText) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }
+        )
+
+        // sequenza di rettangoli premuti
+        Text(
+            text = stringResource(R.string.none),
+            modifier = Modifier.constrainAs(sequenceText) {
+                end.linkTo(parent.end)
+                start.linkTo(sizeText.end, margin = 16.dp)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondScreenPreview() {
+    SimonTheme {
+        SecondScreen()
+    }
 }
