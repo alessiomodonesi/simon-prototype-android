116e92a24f3d298ef0f850a8cb23a6b44ea83032
alessiomodonesi <alessiomodo2004@gmail.com>
2026-05-04 17:13:57 +0200

    ora il ViewModel interroga il DB solo se c'è un activity in ascolto, evitando di fectchare i dati ogni volta che un VM viene istanziato

--- app/src/main/java/it/unipd/dei/esp2526/simon/ViewModel.kt
+++ app/src/main/java/it/unipd/dei/esp2526/simon/ViewModel.kt
@@ -3,13 +3,12 @@ package it.unipd.dei.esp2526.simon
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.room.Room
 import it.unipd.dei.esp2526.simon.data.AppDatabase
 import it.unipd.dei.esp2526.simon.data.GameRecord
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 /**
@@ -20,34 +19,27 @@ import kotlinx.coroutines.launch
  * si occupa di esporre i dati persistenti alla UI non appena l'app viene riaperta.
  * https://developer.android.com/topic/libraries/architecture/viewmodel
  */
-class GameViewModel(application: Application) : AndroidViewModel(application) {
+class ViewModel(application: Application) : AndroidViewModel(application) {
     // utilizzo il singleton getDatabase() invece di chiamare Room.databaseBuilder qui
     private val dao = AppDatabase.getDatabase(application).gameDao()
 
+    // crea lo StateFlow direttamente dalla query di Room
     // stato reattivo che contiene la cronologia per la HistoryActivity
     // https://developer.android.com/kotlin/flow/stateflow-and-sharedflow
-    private val _history = MutableStateFlow<List<GameRecord>>(emptyList())
-    val history: StateFlow<List<GameRecord>> = _history.asStateFlow()
-
-    // appena il ViewModel viene creato, carica lo storico dal DB
-    init {
-        loadHistory()
-    }
-
-    // recupera i dati usando un thread secondario (Dispatchers.IO)
-    // Dispatchers.IO indicates that this coroutine should be executed on a thread reserved for I/O operations.
-    private fun loadHistory() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _history.value = dao.getAllGames() // chiamo la fun getAllGames() nel Dao
-        }
-    }
+    val history: StateFlow<List<GameRecord>> = dao.getAllGames()
+        .stateIn(
+            scope = viewModelScope,
+            // interroga il DB solo se c'è almeno un'Activity in ascolto
+            started = SharingStarted.WhileSubscribed(5000L),
+            initialValue = emptyList()
+        )
 
     // inserisce una nuova partita e aggiorna la lista
+    // Dispatchers.IO indicates that this coroutine should be executed on a thread reserved for I/O operations.
     fun insertGame(maxLength: Int, sequence: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val newRecord = GameRecord(maxLength = maxLength, sequence = sequence)
             dao.insertGame(newRecord) // chiamo la fun insertGame() nel Dao
-            loadHistory() // poi aggiorno la lista
         }
     }
 
