5588d334c1268cdb8ad1b322a6407eccb47a202c
alessiomodonesi <alessiomodo2004@gmail.com>
2026-05-06 15:59:33 +0200

    refuso

--- app/src/main/java/it/unipd/dei/esp2526/simon/DetailActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2526/simon/DetailActivity.kt
@@ -1,6 +1,5 @@
 package it.unipd.dei.esp2526.simon
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -31,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
--- app/src/main/java/it/unipd/dei/esp2526/simon/GameActivity.kt
+++ app/src/main/java/it/unipd/dei/esp2526/simon/GameActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
-import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
