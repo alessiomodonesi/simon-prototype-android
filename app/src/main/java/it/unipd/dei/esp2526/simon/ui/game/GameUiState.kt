f91ad662f791309be95d7e70c84afd970e7fff4b
alessiomodonesi <alessiomodo2004@gmail.com>
2026-05-28 13:02:14 +0200

    passato da Serializable a Parcelable per GameUiState

--- app/src/main/java/it/unipd/dei/esp2526/simon/ui/game/GameUiState.kt
+++ app/src/main/java/it/unipd/dei/esp2526/simon/ui/game/GameUiState.kt
@@ -1,8 +1,15 @@
 package it.unipd.dei.esp2526.simon.ui.game
 
-import java.io.Serializable
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-/** struttura dati di supporto per gestire gli stati della partita */
+/**
+ * struttura dati di supporto per gestire gli stati della partita.
+ * utilizza Parcelable (tramite l'annotazione @Parcelize) per garantire
+ * una serializzazione estremamente efficiente e a bassa latenza all'interno
+ * dei Bundle di sistema.
+ */
+@Parcelize
 data class GameUiState(
     val computerSequence: List<String> = emptyList(), // stato per tenere traccia della sequenza generata dal computer
     val userSequence: List<String> = emptyList(), // stato per tenere traccia della sequenza riprodotta dall'utente
@@ -12,4 +19,4 @@ data class GameUiState(
     val isGameOver: Boolean = false, // stato per gestire la sconfitta dell'utente
     val activeColor: String? = null, // stato per il colore attualmente illuminato
     val computerPlaybackIndex: Int = 0 // stato per l'indice della sequenza del computer
-) : Serializable
+) : Parcelable
